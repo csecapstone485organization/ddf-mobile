3f5b873c34a76930047e3bc779e91bb6168262f7
DrCopernicus <nicholas.copernicus.lee@gmail.com>
2017-03-23 12:27:05 -0700

    Fixes submit button fetch results.

--- App/components/EditQueryForm.js
+++ App/components/EditQueryForm.js
@@ -4,9 +4,9 @@ import { Field, reduxForm } from 'redux-form'
 import store from '../store/store'
 
 type Props = {
-    saveQuery: Function,
-    fetchResults: Function,
-    nextScreen: Function
+  saveQuery: Function,
+  fetchResults: Function,
+  nextScreen: Function
 }
 
 const EditQueryForm = (props: Props) => {
@@ -23,66 +23,66 @@ const EditQueryForm = (props: Props) => {
 
   return (
   <View style={styles.QueryInputContainer}>
-		<TextInput style={styles.TextInputStyle} placeholder={'Query Text'}/>
-		<Picker>
-			<Picker.Item label="Select Match Case Type" value="0" />
-			<Picker.Item label="Yes" value="yes" />
-			<Picker.Item label="No" value="no" />
-		</Picker>
-		<Picker>
-			<Picker.Item label="Select Time Range Type" value="0" />
-			<Picker.Item label="Any" value="any" />
-			<Picker.Item label="After" value="after" />
-			<Picker.Item label="Before" value="before" />
-			<Picker.Item label="Between" value="between" />
-		</Picker>
-		<Picker>
-			<Picker.Item label="Select Location Type" value="0" />		
-			<Picker.Item label="Anywhere" value="anywhere" />
-			<Picker.Item label="Somewhere Specific" value="somewhereSpecific" />
-		</Picker>
-		<Picker>
-			<Picker.Item label="Select Match Type" value="0" />		
-			<Picker.Item label="Any" value="any" />
-			<Picker.Item label="Specific" value="specific" />
-		</Picker>
-		<Picker>
-			<Picker.Item label="Select Sort Attribute Type" value="0" />		
-		</Picker>
-		<Picker>
-			<Picker.Item label="Select Sort Direction Type" value="0" />		
-		</Picker>
-		<View style={styles.buttons}>
-		<Button onPress={() => nextScreen()}
-			title='Save'
-			accessibilityLabel='Submit'>
-			Submit
-		</Button>
-		</View>
-	</View>
+    <TextInput style={styles.TextInputStyle} placeholder={'Query Text'}/>
+    <Picker>
+      <Picker.Item label="Select Match Case Type" value="0" />
+      <Picker.Item label="Yes" value="yes" />
+      <Picker.Item label="No" value="no" />
+    </Picker>
+    <Picker>
+      <Picker.Item label="Select Time Range Type" value="0" />
+      <Picker.Item label="Any" value="any" />
+      <Picker.Item label="After" value="after" />
+      <Picker.Item label="Before" value="before" />
+      <Picker.Item label="Between" value="between" />
+    </Picker>
+    <Picker>
+      <Picker.Item label="Select Location Type" value="0" />      
+      <Picker.Item label="Anywhere" value="anywhere" />
+      <Picker.Item label="Somewhere Specific" value="somewhereSpecific" />
+    </Picker>
+    <Picker>
+      <Picker.Item label="Select Match Type" value="0" />     
+      <Picker.Item label="Any" value="any" />
+      <Picker.Item label="Specific" value="specific" />
+    </Picker>
+    <Picker>
+      <Picker.Item label="Select Sort Attribute Type" value="0" />        
+    </Picker>
+    <Picker>
+      <Picker.Item label="Select Sort Direction Type" value="0" />        
+    </Picker>
+    <View style={styles.buttons}>
+    <Button onPress={onPress}
+      title='Save'
+      accessibilityLabel='Submit'>
+      Submit
+    </Button>
+    </View>
+    </View>
   )
 }
 
 
 const styles = StyleSheet.create({
-	QueryInputContainer: {
-        flex: 1,
-		flexDirection: 'column',
-		justifyContent: 'space-between',
-    },
-	TextInputStyle: {
-		height: 80,
-		fontSize: 20,
-		textAlign: 'center',
-	},
-	buttons: {
-		alignSelf: 'center',
-		width: 300,
-		height: 100,
-	},
-	labels: {
-		height: 20,
-	},
+  QueryInputContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  TextInputStyle: {
+    height: 80,
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  buttons: {
+    alignSelf: 'center',
+    width: 300,
+    height: 100,
+  },
+  labels: {
+    height: 20,
+  },
 })
 
 EditQueryForm = reduxForm({
