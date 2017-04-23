1cc4ea499d1c0064a1570292926faf47c08f0bd7
JCDJulian <julian.deocampo@gmail.com>
2017-04-23 12:54:04 -0700

    Log-in styling

--- App/components/LoginPrompt.js
+++ App/components/LoginPrompt.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native'
+import { View,Image, TextInput, Button, StyleSheet } from 'react-native'
 
 type Props = {
   nextScreen: Function
@@ -9,36 +9,46 @@ const LoginPrompt = (props: Props) => {
   const { nextScreen } = props
   return (
     <View style={styles.loginContent}>
-        <TextInput style={styles.textBox} placeholder={'Username'}/>
-        <TextInput style={styles.textBox} placeholder={'Password'}/>
-        <View style={styles.button}>
-			<Button onPress={() => nextScreen()}
-			title='Login'
-			accessibilityLabel='Login'>
-			Login
-			</Button>
-		</View>
+      <Image source={require('../resources/images/ddf_logo.png')}
+          style={styles.image}
+      />
+      <TextInput style={styles.textBox} placeholder={'Username'}/>
+      <TextInput style={styles.textBox} placeholder={'Password'}/>
+      <View style={styles.button}>
+        <Button onPress={() => nextScreen()}
+          title='Login'
+          accessibilityLabel='Login'
+          color="#414770"
+          >
+          Login
+        </Button>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-    loginContent: {
-        flex: 1,
-		flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-	button: {
-		width: 100,
-		height: 30,
-	},
-    textBox: {
-		textAlign: 'center',
-        width: 200,
-        height: 80,
-		fontSize: 30
-    }
+  loginContent: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor:'#DCE0D9'
+  },
+  textBox: {
+    textAlign: 'left',
+    width: 350,
+    height: 50,
+    fontSize: 30,
+    color:'black',
+    paddingLeft: 25,
+    paddingRight: 25
+  },
+  image: {
+    width:261,
+    height:112,
+    marginBottom: 40
+  }
 });
 
 
